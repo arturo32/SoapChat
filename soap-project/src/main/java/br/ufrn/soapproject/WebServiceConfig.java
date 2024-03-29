c61591923d8a0d9bb77c0a9068825f4e429916f9
Artur Santiago <artursantiago2001@hotmail.com>
2024-03-29 01:36:27 +0000

    feat: add cors config

--- soap-project/src/main/java/br/ufrn/soapproject/WebServiceConfig.java
+++ soap-project/src/main/java/br/ufrn/soapproject/WebServiceConfig.java
@@ -1,10 +1,14 @@
 package br.ufrn.soapproject;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
@@ -36,5 +40,25 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema chatSchema() {
         return new SimpleXsdSchema(new ClassPathResource("templates/chat.xsd"));
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        bean.setOrder(0);
+
+        return bean;
     }
 }
